aabd5eb1aeca268f4f56e5046b7ca56d7a969154
Sam Mulube <sam@umbrellium.co.uk>
2019-01-18 00:49:12 +0000

    Script creates and deletes policies now.

--- integration_test.py
+++ integration_test.py
@@ -1,26 +1,152 @@
 #!/usr/bin/env python
 
 import argparse
+import logging
+import requests
+import sys
+import pprint
 
-help = """
-Execute a series of integration tests against DECODE components in order to
-verify they are behaving as expected.  When executed this command runs the
-following sequence of steps:
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
-    * create a policy and verify we can read this policy back from the policy
-      store
-    * create an encrypted stream for a device that applies this policy
-    * read some data from the encrypted datastore for the policy
-    * decrypt the data using zenroom and display this to the screen
-    * clean up all created resources"""
+POLICYSTORE_PREFIX = '/twirp/decode.iot.policystore.PolicyStore/'
 
-parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=help)
-parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='enable verbose mode', default=False)
-parser.add_argument('--device-token', dest='device_token', help='device token we wish to use for testing', required=True)
-parser.add_argument('--datastore', dest='datastore', help='URL of the encrypted datastore', default='http://localhost:8080')
-parser.add_argument('--policystore', dest='policystore', help='URL of the policy store', default='http://localhost:8082')
-parser.add_argument('--encoder', dest='encoder', help='URL of the stream encoder', default='http://localhost:8081')
-args = parser.parse_args()
+def main():
+    help = """
+    Execute a series of integration tests against DECODE components in order to
+    verify they are behaving as expected.  When executed this command runs the
+    following sequence of steps:
 
-print(args)
+        * create a policy and verify we can read this policy back from the policy
+          store
+        * create an encrypted stream for a device that applies this policy
+        * read some data from the encrypted datastore for the policy
+        * decrypt the data using zenroom and display this to the screen
+        * clean up all created resources"""
 
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, description=help)
+    parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='enable verbose mode', default=False)
+    parser.add_argument('--device-token', dest='device_token', help='device token we wish to use for testing', required=True)
+    parser.add_argument('--datastore', dest='datastore', help='URL of the encrypted datastore', default='http://localhost:8080')
+    parser.add_argument('--policystore', dest='policystore', help='URL of the policy store', default='http://localhost:8082')
+    parser.add_argument('--encoder', dest='encoder', help='URL of the stream encoder', default='http://localhost:8081')
+    parser.set_defaults(func=run)
+    args = parser.parse_args()
+    args.func(args)
+
+def run(args):
+    """This is the list of operations this integration test performs.
+
+    Within this test script, we create a policy with a generated key/pair using
+    Zenroom, we then verify that we can read back the policy from the
+    policystore, then create an encrypted stream on the stream encoder. We then
+    verify that we can read values for the policy back from the datastore, and
+    finally we verify that are able to decrypt the written data again using
+    Zenroom.
+
+    Once the test completes we then try and delete all created resources."""
+
+    logging.info('Starting integration test')
+
+    # read configuration from our args
+    device_token = args.device_token
+    policystore_url = args.policystore
+
+    # create a policy
+    policy = create_policy(policystore_url, create_policy_request())
+
+    # read the policies back from the policystore
+    policies = list_policies(policystore_url)
+
+    # verify the list of policies contains the policy we initially created
+    if policy['policy_id'] not in list(map(lambda p: p['policy_id'], policies['policies'])):
+        sys.exit(f'Policy {policy["policy_id"]} not found in list read from policystore')
+
+    # delete the created policy
+    delete_policy(policystore_url, policy)
+
+
+def create_policy_request():
+    """Return a static configuration for a policy"""
+    return {
+                'public_key': 'BBLewg4VqLR38b38daE7Fj\/uhr543uGrEpyoPFgmFZK6EZ9g2XdK\/i65RrSJ6sJ96aXD3DJHY3Me2GJQO9\/ifjE=',
+                'label': 'Integration Test Policy',
+                'operations': [
+                    {
+                        'sensor_id': 10,
+                        'action': 'SHARE',
+                    },
+                    {
+                        'sensor_id': 53,
+                        'action': 'BIN',
+                        'bins': [30.0,60.0,90.0]
+                    },
+                    {
+                        'sensor_id': 55,
+                        'action': 'MOVING_AVG',
+                        'interval': 300
+                    }
+                ]
+            }
+
+def headers():
+    """Return static headers that all requests need"""
+    return {'user-agent': 'integration-tester', 'content-type': 'application/json'}
+
+def create_policy(policystore_url, create_policy_request):
+    """Create a new entitlement policy.
+
+    This sends a request to the policystore to create a new policy using the
+    static configuration defined previously in this script."""
+
+    logging.info('Creating policy')
+
+    create_url = policystore_url + POLICYSTORE_PREFIX + 'CreateEntitlementPolicy'
+
+    r = requests.post(create_url, headers=headers(), json=create_policy_request)
+    if r.status_code != 200:
+        sys.exit(f'Unexpected response: {r.status_code}')
+
+    resp = r.json()
+
+    logging.info(f'Generated policy ID: {resp["policy_id"]}')
+
+    return r.json()
+
+def delete_policy(policystore_url, policy):
+    """Delete existing entitlement policy.
+
+    This sends a request to the policystore to delete an existing policy. This
+    requires the use of the generated ID and token from a previous create
+    call."""
+
+    logging.info('Deleting policy')
+
+    delete_url = policystore_url + POLICYSTORE_PREFIX + 'DeleteEntitlementPolicy'
+
+    r = requests.post(delete_url, headers=headers(), json=policy)
+    if r.status_code != 200:
+        sys.exit(f'Unexpected response: {r.status_code}')
+
+def list_policies(policystore_url):
+    """List available entitlement policies
+
+    This sends a request to the policystore to read a list of all available
+    policies."""
+
+    logging.info('Listing policies')
+
+    list_url = policystore_url + POLICYSTORE_PREFIX + 'ListEntitlementPolicies'
+
+    r = requests.post(list_url, headers=headers(), json={})
+    if r.status_code != 200:
+        sys.exit(f'Unexpected response: {r.status_code}')
+
+    resp = r.json()
+    logging.info('Policies retrieved')
+
+    pprint.pprint(resp)
+
+    return resp
+
+if __name__=="__main__":
+    main()
